7532f38f9a1faf1605257918cb641c413ea58e82
Filip Pytloun <filip@pytloun.cz>
2017-01-03 18:23:04 +0100

    Binary-only build

--- debian.groovy
+++ debian.groovy
@@ -27,7 +27,7 @@ def buildBinary(file, image="debian:sid") {
     def img = docker.image(image)
 
     workspace = getWorkspace()
-    sh("docker run -e DEBIAN_FRONTEND=noninteractive -v ${workspace}:${workspace} -w ${workspace} --rm=true --privileged ${image} /bin/bash -c 'apt-get update && apt-get install -y build-essential devscripts && dpkg-source -x ${file} build-area/${pkg} && cd build-area/${pkg} && dpkg-checkbuilddeps 2>&1|rev|cut -d : -f 1|rev|sed \"s,(.*),,g\"|xargs apt-get install -y && debuild --no-lintian -uc -us'")
+    sh("docker run -e DEBIAN_FRONTEND=noninteractive -v ${workspace}:${workspace} -w ${workspace} --rm=true --privileged ${image} /bin/bash -c 'apt-get update && apt-get install -y build-essential devscripts && dpkg-source -x ${file} build-area/${pkg} && cd build-area/${pkg} && dpkg-checkbuilddeps 2>&1|rev|cut -d : -f 1|rev|sed \"s,(.*),,g\"|xargs apt-get install -y && debuild --no-lintian -uc -us -b'")
 }
 
 /*
