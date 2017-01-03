92955037127816a8a2c7d41f1d0219cd21b73a16
Filip Pytloun <filip@pytloun.cz>
2017-01-03 17:09:33 +0100

    Don't use cloudbees docker plugin for debian

--- debian.groovy
+++ debian.groovy
@@ -1,3 +1,21 @@
+/**
+ * Return workspace.
+ * Currently implemented by calling pwd so it won't return relevant result in
+ * dir context
+ */
+def getWorkspace() {
+    def workspace = sh script: 'pwd', returnStdout: true
+    workspace = workspace.trim()
+    return workspace
+}
+
+def cleanup(image="debian:sid") {
+    def img = docker.image(image)
+
+    workspace = getWorkspace()
+    sh("docker run -e DEBIAN_FRONTEND=noninteractive -v ${workspace}:${workspace} -w ${workspace} --rm=true --privileged ${image} /bin/bash -c 'rm -rf build-area || true'")
+}
+
 /*
  * Build binary Debian package from existing dsc
  *
@@ -7,13 +25,9 @@
 def buildBinary(file, image="debian:sid") {
     def pkg = file.split('/')[-1].split('_')[0]
     def img = docker.image(image)
-    img.withRun('-u root -e DEBIAN_FRONTEND=noninteractive') {c ->
-        sh("apt-get update && apt-get install -y build-essential")
-        sh("test -d build || mkdir build")
-        sh("dpkg-source -x ${file} build/${pkg}")
-        sh("cd build/${pkg}; apt-get update; dpkg-checkbuilddeps 2>&1|rev|cut -d : -f 1|rev|sed 's,(.*),,g'|xargs apt-get install -y")
-        sh("cd build/${pkg}; debuild --no-lintian -uc -us")
-    }
+
+    workspace = getWorkspace()
+    sh("docker run -e DEBIAN_FRONTEND=noninteractive -v ${workspace}:${workspace} -w ${workspace} --rm=true --privileged ${image} /bin/bash -c 'apt-get update && apt-get install -y build-essential devscripts && dpkg-source -x ${file} build-area/${pkg} && cd build-area/${pkg} && dpkg-checkbuilddeps 2>&1|rev|cut -d : -f 1|rev|sed \"s,(.*),,g\"|xargs apt-get install -y && debuild --no-lintian -uc -us'")
 }
 
 /*
@@ -23,7 +37,15 @@ def buildBinary(file, image="debian:sid") {
  * @param image Image name to use for build (default debian:sid)
  */
 def buildSource(dir, image="debian:sid") {
-    if (new File("${dir}/.git").exists()) {
+    def isGit
+    try {
+        sh("test -d ${dir}/.git")
+        isGit = true
+    } catch (Exception e) {
+        isGit = false
+    }
+
+    if (isGit == true) {
         buildSourceGbp(dir, image)
     } else {
         buildSourceUscan(dir, image)
@@ -38,11 +60,8 @@ def buildSource(dir, image="debian:sid") {
  */
 def buildSourceUscan(dir, image="debian:sid") {
     def img = docker.image(image)
-    img.withRun('-u root -e DEBIAN_FRONTEND=noninteractive') {c ->
-        sh("apt-get update && apt-get install -y build-essential")
-        sh("cd ${dir} && uscan --download-current-version")
-        sh("cd ${dir} && dpkg-buildpackage -S -nc -uc -us")
-    }
+    workspace = getWorkspace()
+    sh("docker run -e DEBIAN_FRONTEND=noninteractive -v ${workspace}:${workspace} -w ${workspace} --rm=true --privileged ${image} /bin/bash -c 'apt-get update && apt-get install -y build-essential devscripts && cd ${dir} && uscan --download-current-version && dpkg-buildpackage -S -nc -uc -us'")
 }
 
 /*
@@ -53,10 +72,8 @@ def buildSourceUscan(dir, image="debian:sid") {
  */
 def buildSourceGbp(dir, image="debian:sid") {
     def img = docker.image(image)
-    img.withRun('-u root -e DEBIAN_FRONTEND=noninteractive') {c ->
-        sh("apt-get update && apt-get install -y build-essential git-buildpackage")
-        sh("cd ${dir} && gbp buildpackage --git-notify=false --git-ignore-branch --git-ignore-new -S -uc -us")
-    }
+    workspace = getWorkspace()
+    sh("docker run -e DEBIAN_FRONTEND=noninteractive -v ${workspace}:${workspace} -w ${workspace} --rm=true --privileged ${image} /bin/bash -c 'apt-get update && apt-get install -y build-essential git-buildpackage && cd ${dir} && gbp buildpackage -nc --git-force-create --git-notify=false --git-ignore-branch --git-ignore-new --git-verbose -S -uc -us'")
 }
 
 /*
@@ -68,10 +85,8 @@ def buildSourceGbp(dir, image="debian:sid") {
  */
 def runLintian(changes, profile="debian", image="debian:sid") {
     def img = docker.image(image)
-    img.withRun('-u root -e DEBIAN_FRONTEND=noninteractive') {c ->
-        sh("apt-get update && apt-get install -y lintian")
-        sh("lintian --no-tag-display-limit -Ii -E --pedantic --profile=${profile} '${changes}'")
-    }
+    workspace = getWorkspace()
+    sh("docker run -e DEBIAN_FRONTEND=noninteractive -v ${workspace}:${workspace} -w ${workspace} --rm=true --privileged ${image} /bin/bash -c 'apt-get update && apt-get install -y lintian && cd ${dir} && lintian --no-tag-display-limit -Ii -E --pedantic --profile=${profile} ${changes}'")
 }
 
 return this;
